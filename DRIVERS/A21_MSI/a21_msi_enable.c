541a654ab4df1e7db9439f8c83bb290550c07a9b
thomas.schnuerer <thomas.schnuerer@men>
2015-03-05 10:38:32 +0100

    A21 MSI native driver necessary for customer specific A21 variant, added DRIVER/A21_MSI folder LIBSRC/DRIVERS/NATIVE/nat_module.c still contained MSI activation code, removed

---
+++ DRIVERS/A21_MSI/a21_msi_enable.c
@@ -0,0 +1,103 @@
+/*********************  P r o g r a m  -  M o d u l e ***********************
+ *
+ *         Name: nat_module.c
+ *      Project: MDIS4LINUX
+ *
+ *       Author: ts
+ *
+ *     Description: Module stuff for CHAMELEON MDIS module
+ *
+ *
+ ****************************************************************************/
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/pci.h>
+
+#define A21_MSI_BLOCK_SIZE    32 
+
+/* not present in pci_ids.h of kernel 3.4.18 yet... */
+#define PCI_VENDOR_ID_MEN_MIKRO         0x1a88
+#define PCI_DEVICE_ID_MEN_MIKRO_CHAM    0x4d45
+
+/*****************************  A21_MSI_enable ******************************
+ *
+ *  Description:  dummy helper to create an artificial dependency so this
+ *                is loaded before A21 BBIS is opened
+ *---------------------------------------------------------------------------
+ *  Input......:  -
+ *  Output.....:  returns: 0 on success
+ *  Globals....:  -
+ ****************************************************************************/
+
+int A21_MSI_enable(void)
+{
+  return 0;
+}
+EXPORT_SYMBOL(A21_MSI_enable);
+
+/*****************************  init_module  *********************************
+ *
+ *  Description:  Called when module is loaded by insmod
+ *---------------------------------------------------------------------------
+ *  Input......:  -
+ *  Output.....:  returns: 0 on success
+ *  Globals....:  -
+ ****************************************************************************/
+int mod_init(void)
+{
+	struct pci_dev *pdev = NULL;
+	int error;
+	int nvec;
+	printk( KERN_INFO "MEN " COMP_NAME " init_module.\n");
+
+	pdev = pci_get_device(PCI_VENDOR_ID_MEN_MIKRO, PCI_DEVICE_ID_MEN_MIKRO_CHAM, pdev);
+	if (!pdev) {
+		printk(KERN_ERR "PCI device not found\n");
+		return -ENODEV;
+	}
+
+	if (pdev->msi_enabled) {
+	  printk( KERN_INFO "MSIs enabled already, leaving.\n");
+	  goto out;
+	}
+
+	error = pci_enable_device(pdev);
+	if (error) {
+		printk(KERN_ERR "pci_enable_device failed\n");
+		return -ENODEV;
+	}
+
+	pci_set_master(pdev);
+
+	if ( pci_msi_enabled()) {
+	  if((nvec = pci_enable_msi_block(pdev, A21_MSI_BLOCK_SIZE ))) {
+	    if (pci_enable_msi_block(pdev, nvec) < 0) {
+	      printk(KERN_ERR " *** failed to request MSI, falling back to legacy IRQs\n");
+	    }
+	  }
+	}
+	printk( KERN_INFO "MSIs enabled successfully.\n");
+out:
+	return 0;
+}
+
+/*****************************  cleanup_module  ******************************
+ *
+ *  Description:  Called before module is unloaded by rmmod
+ *---------------------------------------------------------------------------
+ *  Input......:  -
+ *  Output.....:  -
+ *  Globals....:  -
+ ****************************************************************************/
+void mod_exit(void)
+{
+	printk( KERN_INFO "MEN " COMP_NAME " cleanup_module\n");
+}
+
+module_init( mod_init );
+module_exit( mod_exit );
+MODULE_DESCRIPTION( COMP_NAME "MDIS module");
+MODULE_AUTHOR("Thomas Schnuerer <thomas.schnuerer@men.de>");
+MODULE_LICENSE("GPL");
---
+++ DRIVERS/A21_MSI/driver.mak
@@ -0,0 +1,21 @@
+#**************************  M a k e f i l e ********************************
+#  
+#         Author: ts
+#  
+#    Description: makefile descriptor for A21 MSI enabler helper
+#                      
+#---------------------------------[ History ]---------------------------------
+#
+#*****************************************************************************
+
+MAK_NAME=lx_a21_msi_enable
+
+MAK_LIBS=
+
+MAK_SWITCH =
+
+MAK_INCL=
+
+MAK_INP1=a21_msi_enable$(INP_SUFFIX)
+
+MAK_INP=$(MAK_INP1)
