444321599a61146dc6a1a5e924d424244b5d96d4
thomas.schnuerer <thomas.schnuerer@men>
2016-04-27 15:03:46 +0200

    G25A was not proberly detected in scan_system (is named G025A in EEPROM), updated KNOWN_CPU string

--- scan_system.sh
+++ scan_system.sh
@@ -39,7 +39,7 @@ MOD_DIR=/lib/modules/`uname -r`
 
 # currently detected CPU boards. ADD NEW BOARDS HERE!
 # also take care for special (native) driver adding etc.
-CPU_KNOWN_BOARDS="SC25 SC24 F011 F11S F14- F014 F15- F015 F17- F017 F075 F75P F19P F19C F019 F21P F22P F23P F21C F021 XM01 MM01 G20- G22- G22A G23- G23A G25- G25A "
+CPU_KNOWN_BOARDS="SC25 SC24 F011 F11S F14- F014 F15- F015 F17- F017 F075 F75P F19P F19C F019 F21P F22P F23P F21C F021 XM01 MM01 G20- G22- G22A G23- G23A G25- G25A G025"
 
 # which SMB adresses to scan for CPU ID eeproms
 ID_EEPROM_ADRESSES="0x57 0x55"
@@ -1061,14 +1061,14 @@ case $main_cpu in
 		bCreateXm01bcDrv=1
 	    ;;
 
-    G25-|G25A)
+    G25-|G25A|G025)
 	    wiz_model_cpu=G25A
 	    wiz_model_smb=SMBPCI_ICH
 	    G_primPciPath=0x1c
 	    wiz_model_busif=7
-		add_xm01bc_support
+	    add_xm01bc_support
 	    add_z001_io_support
-		bCreateXm01bcDrv=1
+	    bCreateXm01bcDrv=1
         ;;
     *)
 		echo "No MEN CPU type found!"
