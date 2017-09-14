fbc6823b11a29ce0ab6db39c194d379df616a0c4
thomas.schnuerer <thomas.schnuerer@men>
2017-09-14 14:42:54 +0200

    Added time and transfer rate calculation to vme4l_rwex, option -l for aligned memory and option -d to supress read memory dump

--- TOOLS/VME4L_API/VME4L_RWEX/vme4l_rwex.c
+++ TOOLS/VME4L_API/VME4L_RWEX/vme4l_rwex.c
@@ -13,18 +13,6 @@
  */
 /*-------------------------------[ History ]---------------------------------
  *
- * $Log: vme4l_rwex.c,v $
- * Revision 1.3  2009/06/03 19:19:53  rt
- * R: 1.) No support for VME addresses > 0xFFFFFFFF.
- * M: 1.) Use strtoull for VME address.
- *
- * Revision 1.2  2009/04/30 21:18:05  rt
- * R: 1.) Print better error message.
- * M: 1.) Added comment.
- *
- * Revision 1.1  2003/12/15 15:02:28  kp
- * Initial Revision
- *
  *---------------------------------------------------------------------------
  * (c) Copyright 2017 by MEN mikro elektronik GmbH, Nuremberg, Germany
  ******************************************************************************/
@@ -43,14 +31,15 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-static const char RCSid[]="$Id: vme4l_rwex.c,v 1.3 2009/06/03 19:19:53 rt Exp $";
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
 #include <signal.h>
+#include <unistd.h>
+#include <malloc.h>
 #include <errno.h>
+#include <sys/time.h>
 #include <MEN/vme4l.h>
 #include <MEN/vme4l_api.h>
 #include <MEN/men_typs.h>
@@ -58,34 +47,35 @@ static const char RCSid[]="$Id: vme4l_rwex.c,v 1.3 2009/06/03 19:19:53 rt Exp $"
 #include <MEN/usr_oss.h>
 
 /*--------------------------------------+
-|   DEFINES                             |
-+--------------------------------------*/
-#define CHK(expression) \
- if( !(expression)) {\
-	 printf("\n*** Error during: %s\nfile %s\nline %d\n", \
-      #expression,__FILE__,__LINE__);\
-      printf("%s\n",strerror(errno));\
-     goto ABORT;\
- }
+  |   DEFINES                             |
+  +--------------------------------------*/
+#define CHK(expression)											\
+	if( !(expression)) {										\
+		printf("\n*** Error during: %s\nfile %s\nline %d\n",	\
+			   #expression,__FILE__,__LINE__);					\
+		printf("%s\n",strerror(errno));							\
+		goto ABORT;												\
+	}
+
+#define _1M 	1000000  			/* for us->s */
+#define _1MB 	(1*1024*1024)		/* for MB/s */
 
 /*--------------------------------------+
-|   TYPDEFS                             |
-+--------------------------------------*/
+  |   TYPDEFS                             |
+  +--------------------------------------*/
 
 /*--------------------------------------+
-|   GLOBALS                             |
-+--------------------------------------*/
-
+  |   GLOBALS                             |
+  +--------------------------------------*/
 
 /*--------------------------------------+
-|   PROTOTYPES                          |
-+--------------------------------------*/
+  |   PROTOTYPES                          |
+  +--------------------------------------*/
 static void MemDump(
     uint8_t *buf,
     uint32_t n,
     uint32_t fmt
 	);
-
 
 static void usage(int excode)
 {
@@ -98,12 +88,14 @@ static void usage(int excode)
 	printf("-x            use swap mode                        [0]\n");
 	printf("-r            read from VME space into CPU\n");
 	printf("-w            write from CPU to VME space\n");
+	printf("-d            do *NOT* dump data (if reading big size)\n");
+	printf("-l            use page-aligned memory as buffer\n");
 	exit(excode);
 }
 
 static void SigHandler( int sigNum )
 {
-
+	return; /* nothing  */
 }
 
 /**********************************************************************/
@@ -114,23 +106,23 @@ static void SigHandler( int sigNum )
 int main( int argc, char *argv[] )
 {
 	int fd, rv, i;
-	uint8_t *buf;
+	void *buf=NULL;
 	VME4L_SPACE spc=0;
 	vmeaddr_t vmeAddr;
 	u_int32 startaddr= 0xffffffff;
 	int accWidth=4;
 	size_t size = 0xffffffff;
 	char *optp=NULL;
-	int doRead=-1;
-	int swapMode = 0;
-	int startVal = 0;
+	int opt_read=-1, opt_dump=1, opt_swapmode = 0, startVal = 0, opt_align=0;
+	struct timeval t1, t2;
+	float transferRate=0.0, deltaSec=0.0;
 
 	if( UTL_TSTOPT("?") || (argc == 1) )
 		usage(0);
 
 	/*-----------------------------+
-    |  Parse command line options  |
-    +-----------------------------*/
+	  |  Parse command line options  |
+	  +-----------------------------*/
 	for(i=1; i<argc; i++ )
 		if( *argv[i]!='-'){
 			if( startaddr == 0xffffffff )
@@ -149,9 +141,11 @@ int main( int argc, char *argv[] )
 	vmeAddr = startaddr;
 
 	if (UTL_TSTOPT("r"))
-		doRead = 1;
+		opt_read = 1;
 	else if (UTL_TSTOPT("w"))
-		doRead = 0;
+		opt_read = 0;
+
+	opt_dump = (optp=UTL_TSTOPT("d")) ? 0 : 1;
 
 	if( (optp=UTL_TSTOPT("a=")))
 		accWidth=strtoul( optp, NULL, 0 );
@@ -159,30 +153,38 @@ int main( int argc, char *argv[] )
 	if( (optp=UTL_TSTOPT("s=")))
 		spc=strtoul( optp, NULL, 0 );
 
-	swapMode = (optp=UTL_TSTOPT("x")) ? 1 : 0;
+	opt_swapmode = (optp=UTL_TSTOPT("x")) ? 1 : 0;
+	opt_align = (optp=UTL_TSTOPT("l")) ? 1 : 0;
 
 	if( (optp=UTL_TSTOPT("v=")))
 		startVal=strtoul( optp, NULL, 0 );
 
-
-	if (doRead == -1) {
+	if (opt_read == -1) {
 		printf("*** specify either -r or -w!\n");
 		usage(1);
 	}
 
+	if ( opt_align ) {
+		CHK( posix_memalign( &buf, getpagesize(), size ) == 0 );
+	} else {
+		CHK( (buf = malloc( size )) != NULL);
+	}
 
-	CHK( (buf = malloc( size )) != NULL);
-
-	printf("Open space %s, user buffer @ %p \n", VME4L_SpaceName(spc), buf );
+	printf("Open space %s,%suser buffer @ %p\n",
+		   VME4L_SpaceName(spc), opt_align ? " page aligned " : " ", buf );
 
 	CHK( (fd = VME4L_Open( spc )) >= 0 /*node /dev/vme4l_<spc> must exist*/ );
 
-	CHK( VME4L_SwapModeSet( fd, swapMode ) == 0 );
+	CHK( VME4L_SwapModeSet( fd, opt_swapmode ) == 0 );
 
-	if( doRead )
+	if( opt_read )
 	{
+		/* measure time right before and after VME access, mem dumps can take time */
+		gettimeofday( &t1, 0 );
 		CHK( (rv = VME4L_Read( fd, vmeAddr, accWidth, size, buf, VME4L_RW_NOFLAGS )) >=0);
-		MemDump( buf, rv, 1 );
+		gettimeofday( &t2, 0 );
+		if ( opt_dump )
+			MemDump( buf, rv, 1 );
 	}
 	else {
 		uint8_t *p = buf;
@@ -191,12 +193,23 @@ int main( int argc, char *argv[] )
 		for( i=0; i < size; i++ )
 			*p++ = (i + startVal) & 0xff;
 
+		gettimeofday( &t1, 0 );
 		CHK( (rv = VME4L_Write( fd, vmeAddr, accWidth, size, buf, VME4L_RW_NOFLAGS )) >=0);
+		gettimeofday( &t2, 0 );
 	}
 
+	deltaSec = (float)((t2.tv_sec - t1.tv_sec) * _1M + (t2.tv_usec - t1.tv_usec) /* us */)/_1M;
+	transferRate = ((float)size/deltaSec)/_1MB;
+
+	printf("%s finished. Time: %.3f s => transfer rate: %.3f MB/s\n", opt_read ? "read" : "write", deltaSec, transferRate );
+
+	free ( buf );
 	return 0;
 
- ABORT:
+ABORT:
+	if ( buf != NULL )
+	    free ( buf );
+
 	return 1;
 }
 
@@ -204,7 +217,7 @@ static void MemDump(
     uint8_t *buf,
     uint32_t n,
     uint32_t fmt
-)
+	)
 {
     uint8_t *k, *k0, *kmax = buf+n;
     int32_t i;
@@ -215,24 +228,24 @@ static void MemDump(
 
         switch(fmt)                                        /* dump hex: */
         {
-           case 4 : for (k=k0,i=0; i<16; i+=4, k+=4)       /* long aligned */
-                       if (k<kmax)  printf("%08lx ",*(uint32_t*)k);
-                       else         printf("         ");
-                    break;
-           case 2 : for (k=k0,i=0; i<16; i+=2, k+=2)       /* word aligned */
-                       if (k<kmax)  printf("%04x ",*(uint16_t*)k & 0xffff);
-                       else         printf("     ");
-                    break;
-           default: for (k=k0,i=0; i<16; i++, k++)         /* byte aligned */
-                       if (k<kmax)  printf("%02x ",*k & 0xff);
-                       else         printf("   ");
+		case 4 : for (k=k0,i=0; i<16; i+=4, k+=4)       /* long aligned */
+				if (k<kmax)  printf("%08lx ",*(uint32_t*)k);
+				else         printf("         ");
+			break;
+		case 2 : for (k=k0,i=0; i<16; i+=2, k+=2)       /* word aligned */
+				if (k<kmax)  printf("%04x ",*(uint16_t*)k & 0xffff);
+				else         printf("     ");
+			break;
+		default: for (k=k0,i=0; i<16; i++, k++)         /* byte aligned */
+				if (k<kmax)  printf("%02x ",*k & 0xff);
+				else         printf("   ");
         }
 
         for (k=k0,i=0; i<16 && k<kmax; i++, k++)           /* dump ascii */
             if ( *(uint8_t*)k>=32 && *(uint8_t*)k<=127 )
-               printf("%c", *k);
+				printf("%c", *k);
             else
-               printf(".");
+				printf(".");
 
         printf("\n");
     }
