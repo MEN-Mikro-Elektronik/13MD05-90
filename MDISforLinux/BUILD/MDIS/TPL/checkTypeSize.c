20798d2e5a8d3166dfb80fd9e57163eca749bc72
KonradJPE <ktomasik@jpembedded.eu>
2019-09-05 10:24:24 +0200

    Add MEN Copyrights
    Issue 'MDIS license clearing'
    https://github.com/MEN-Mikro-Elektronik/13MD05-90/issues/116

--- MDISforLinux/BUILD/MDIS/TPL/checkTypeSize.c
+++ MDISforLinux/BUILD/MDIS/TPL/checkTypeSize.c
@@ -1,3 +1,6 @@
+ /****************************************************************************
+ * Copyright 2019, MEN Mikro Elektronik GmbH
+ ****************************************************************************/
 /*
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
