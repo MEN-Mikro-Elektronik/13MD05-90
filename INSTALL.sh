84c2d7a549ceb378bd1fd1f5a3450287a74d25cf
KonradJPE <ktomasik@jpembedded.eu>
2019-11-18 11:45:00 +0100

    Remove outdated fields, add license and MEN copyright note

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,7 +2,6 @@
 #*****************************************************************************
 #  
 #         Author: Andreas Geissler
-#          $Date: 2018-06-20 
 #       Revision: 0.1
 #  
 #    Description: Installation script for MDIS5LINUX system package
@@ -10,6 +9,18 @@
 #-----------------------------------------------------------------------------
 # (c) Copyright 2018-2019 by MEN Mikro Elektronik GmbH, Nuremberg, Germany
 #*****************************************************************************
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # warn when using uninitialised variables
 set -u
