f2db5ca2e692a5f7b316022cab778cf8421fe994
Marcin Lassota <mlassota@jpembedded.eu>
2020-04-30 15:59:25 +0200

    Add copyrights

--- MDISforLinux/SCAN_SYSTEM_SCRIPTS/xml_parser.sh
+++ MDISforLinux/SCAN_SYSTEM_SCRIPTS/xml_parser.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#-----------------------------------------------------------------------------
+# Copyright 2020, MEN Mikro Elektronik GmbH
+#*****************************************************************************
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 ### @brief M-Module ID to XML file map
 declare -A mmodFileList
